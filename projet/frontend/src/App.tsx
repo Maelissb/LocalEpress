3861daafc658f98b95e4503f47d9269db485e887
barbot maeliss <barbotmaeliss@gmail.com>
2024-08-19 11:41:00 +0200

    add header

--- projet/frontend/src/App.tsx
+++ projet/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import Homepage from "./components/App/ui/homepage";
 import Header from "./components/Header/header";
 
 function App() {
---
+++ projet/frontend/src/components/App/ui/homepage.tsx
@@ -0,0 +1,12 @@
+import Header from "../../Header/header";
+import '../../../styles/_home.sass'
+
+function Homepage() {
+    return (
+        <div className="home">
+            <Header />
+        </div>
+    );
+}
+
+export default Homepage;
