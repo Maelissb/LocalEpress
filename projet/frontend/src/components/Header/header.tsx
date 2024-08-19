90584d0dd35e9bf685b204636ea56becf9aff8a3
barbot maeliss <barbotmaeliss@gmail.com>
2024-08-19 10:38:47 +0200

    send

---
+++ projet/frontend/src/components/Header/header.tsx
@@ -0,0 +1,14 @@
+import '../../styles/_header.sass'
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1>Local Express</h1>
+            <h2>Vos courses Livrées avec le sourire</h2>
+            <button>Commencer</button>
+        </header>
+    )
+}
+    
+
+export default Header;
